261b8c60e10fb3fd00e997662af8062fe9d161ce
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-11-13 13:37:06 -0600

    Use http client to send data to Ubidots Data API

--- main/main.c
+++ main/main.c
@@ -6,22 +6,40 @@
 
 #include "wifi_setup.h"
 #include "dht22.h"
+#include "http_client.h"
 
 #define DHT_GPIO GPIO_NUM_22
 
+// Ubidots api endpoint and token. Replace 'my-esp32' with 
+// your device label.
+#define API_URL "https://industrial.ubidots.com/api/v1.6/devices/my-esp32"
+#define UBIDOTS_TOKEN "your-ubidots-token"
+
 void sensor_task(void* pvParameter)
 {
-    setDhtGpio(DHT_GPIO);
+    set_dht_gpio(DHT_GPIO);
 
     while (1)
     {
+        // Get sensor read, check for possible error.
         int result = read_dht();
 
         dht_error_handler(result);
 
-        printf("Temperature: %.1f °C, ", getTemperature());
-        printf("Humidity: %.1f %%\n", getHumidity());
+        // Log values to terminal
+        printf("Temperature: %.1f °C, ", get_temperature());
+        printf("Humidity: %.1f %%\n", get_humidity());
 
+        // Get sensor values as JSON string.
+        char json_str_buffer[150];
+        result = sensor_json_data(json_str_buffer, sizeof(json_str_buffer));
+
+        dht_error_handler(result);
+
+        // Make POST request to Ubidots Data API
+        ESP_ERROR_CHECK(http_post_request(API_URL, UBIDOTS_TOKEN, json_str_buffer));
+        
+        // Delay between reads must be of at least 2 seconds.
         vTaskDelay(5000 / portTICK_RATE_MS);
     }
 }
@@ -37,11 +55,13 @@ void app_main(void)
       nvs_status = nvs_flash_init();
     }
 
+    // Verify that nvs is initialized before starting WiFi driver.
     ESP_ERROR_CHECK(nvs_status);
 
     esp_err_t wifi_start_status = wifi_sta_init();
 
     ESP_ERROR_CHECK(wifi_start_status);
 
-    xTaskCreate(&sensor_task, "DHT_Task", 2048, NULL, 3, NULL);
+    // If WiFi connection was successful, start sensor task.
+    xTaskCreate(&sensor_task, "dht_task", 8096, NULL, 5, NULL);
 }
